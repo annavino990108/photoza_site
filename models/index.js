7718fe4ea61533e58d4c059c6e94469b9a36b962
Anna <annavinogradova9901@gmail.com>
2019-06-15 01:42:30 +0300

    v2

---
+++ models/index.js
@@ -0,0 +1,16 @@
+const User = require('./user');
+const File = require('./file');
+const District = require('./geo_district');
+const Region = require('./geo_regions');
+const City = require('./geo_city');
+const Dialog = require('./dialogs');
+const TypePhoto = require('./type_photo');
+module.exports = {
+  User,
+  File,
+  District,
+  Region,
+  City,
+  Dialog,
+  TypePhoto
+};
